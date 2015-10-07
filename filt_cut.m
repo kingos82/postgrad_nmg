457289f0390c74a555a6527873cbc2802a5b1dc4
kingos82 <kingos82@gmail.com>
2015-10-07 16:13:23 -0400

    adding surround
    I added the code for surround filter (not temporal surround yet) and
    consolidated the Gaussian fit into one function fg0 this commit is done
    before I delete the redundant functions and combine the temporal center
    and surround to avoid clutter

--- filt_cut.m
+++ filt_cut.m
@@ -27,76 +27,170 @@ dly=16; p_sz=50;
  AI_flt=spk_mat*(AI_img./sum(AI_spkm(:)));
  
  %% cut
- xyz=[28 26 20 1 16]; %AI 
+ xyz=[28 25 15 1 14]; %AI
+ %xyz=[28 26 20 1 16]; %AI 
  %xyz=[26 24 8 1 9]; %NI
- y=[xyz(1)-xyz(3):xyz(1)+xyz(3)]; x=[xyz(2)-xyz(3):xyz(2)+xyz(3)]; 
- m = zeros(50,50); m(x,y)=1; m=reshape(m, 1, []); m=repmat(m, 16,1);
- AIc=AI_flt.*m; AIc=AIc(xyz(4):xyz(5),:); 
- NIc=NI_flt.*m; NIc=NIc(xyz(4):xyz(5),:); 
- v=[xyz(4):xyz(5)]; dly=length(v);
-%  
-% %% fit Gaussian to filter
-% %surf(reshape(AIc(3,:),50,50));
-% sz=1:50;
-% [C,I] = max(abs(AIc(3,:))); [row,col]=find(reshape([1:2500],50,50)==I);
-% mt=reshape(AIc(3,:),50,50); %STA(sz, sz);
-% 
-% x0(1)=AIc(3,I);%%%amplitude
-% x0(2)=col;
-% x0(3)=row;
-% x0(4)= 1;%sd
-% 
-% 
-% outp = fminsearch(@(x) gf2(x,mt), x0)
-% 
-% A=outp(1);
-% x1=outp(2);
-% y1=outp(3);
-% sx=outp(4);
-% sy=sx;
-% prmtz=[]; outpi=outp; AIf=[];
-% for k=1:16
-%     mtx=reshape(AIc(k,:),50,50);
-%     outp = fminsearch(@(x) gf3(x,mtx,[x1 y1 sx]), A);
-%     prmtz=[prmtz; outp];
-%     A=outp(1); 
-%     [xi, yi] = meshgrid(1:length(mtx));
-%     xp=((xi-x1).^2)./(2.*sx.^2);
-%     yp=((yi-y1).^2)./(2.*sy.^2);
-%     n=A.*exp(-(xp+yp));%./(2.*pi.*sx.*sy)
-%     AIf=[AIf; reshape(n,1,[])];
-%     
-%     
-% end
-% 
-% 
-% 
-% [xi, yi] = meshgrid(1:length(mt));
-% 
-% xp=((xi-x1).^2)./(2.*sx.^2);
-% yp=((yi-y1).^2)./(2.*sy.^2);
-% 
-% n=A.*exp(-(xp+yp));%./(2.*pi.*sx.*sy)
-% 
-% %n=x(1).*fspecial('gaussian', length(sz), x(2)) +x(3);
-% subplot(2,2,1)
-% surf(mt)
-% title('data')
-% 
-% subplot(2,2,2)
-% surf(reshape(AIf(3,:),50,50))
-% title('fit')
-% 
-% subplot(2,2,3)
-% surf(mt-reshape(AIf(3,:),50,50))
-% title('data-fit')
-% 
-% subplot(2,2,4)
-% n=reshape(AIf(3,:),50,50);
-% plot(1:length(sz), [mt(round(y1),:); n(round(y1),:); mt(:,round(x1))'; n(:,round(x1))'])
-% title('x section')
+ x=[xyz(1)-xyz(3):xyz(1)+xyz(3)]; y=[xyz(2)-xyz(3):xyz(2)+xyz(3)]; 
+ m = zeros(50,50); m(y,x)=1; m=reshape(m, 1, []); m=repmat(m, 16,1);
+ AIct=AI_flt.*m; AIc=zeros(size(AIct)); AIc(xyz(4):xyz(5),:)=AIct(xyz(4):xyz(5),:); %% cut AI filter
+ NIct=NI_flt.*m; NIc=zeros(size(NIct)); NIc(xyz(4):xyz(5),:)=NIct(xyz(4):xyz(5),:); %% cut NI filter
+ v=[xyz(4):xyz(5)]; dlyv=length(v);
 
-%   %% projections from orginal filter estimation
+ %% fit Gaussian center spatial filter
+ 
+ G=AIc; i=3;
+ sz=1:50;
+ [C,I] = max(abs(G(i,:))); [row,col]=find(reshape([1:2500],50,50)==I);
+ mt=reshape(G(i,:),50,50); %STA(sz, sz);
+ 
+ x0(1)=G(i,I);%%%amplitude
+ x0(3)=col;
+ x0(4)=row;
+ x0(2)= 1;%sd
+ 
+ outp = fminsearch(@(x) gf0(x,mt, []), x0);
+ 
+ A=outp(1);
+ x1=outp(3);
+ y1=outp(4);
+ sx=outp(2);
+ sy=sx;
+ 
+ %% fit gaussian center temporal filter
+ prmtz=[]; outpi=outp; Gf =zeros(dly, 2500);
+ 
+ 
+ for k=1:dlyv
+     mtx=reshape(G(v(k),:),50,50);
+     outp = fminsearch(@(x) gf0(x,mtx,[x1 y1 sx]), A);
+     prmtz=[prmtz; outp];
+     Ax=outp(1); 
+     [xi, yi] = meshgrid(1:length(mtx));
+     xp=((xi-x1).^2)./(2.*sx.^2);
+     yp=((yi-y1).^2)./(2.*sy.^2);
+     n=Ax.*exp(-(xp+yp));%./(2.*pi.*sx.*sy)
+     Gf(v(k),:)=reshape(n,1,[]);
+ end
+ 
+ %% add surround filter
+  mtx=reshape((G(i,:)-Gf(i,:)),50,50); %STA(sz, sz);
+
+ 
+ outp = fminsearch(@(x) gf0(x,mtx,[x1 y1]), [-A./2 2.*sx]);
+ 
+ As=outp(1);
+ sxf=outp(2);
+ syf=sxf;
+ 
+     xs=((xi-x1).^2)./(2.*outp(2).^2);
+     ys=((yi-y1).^2)./(2.*outp(2).^2);
+     ns=outp(1).*exp(-(xs+ys));%./(2.*pi.*sx.*sy)
+ 
+ 
+ %% calculate projections
+ 
+ prjz=AI_img*AIc';
+ prj=zeros(size(prjz));
+ for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
+ AIA=sum(prj,2);
+ 
+ prjz=AI_img*Gf';
+ prj=zeros(size(prjz));
+ for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
+ AIG=sum(prj,2);
+ 
+ nbn=16;
+[nAIG cAIG eAIG]=NLf082115(AIG, AI_SPK, nbn);
+[nAIA cAIA eAIA]=NLf082115(AIA, AI_SPK, nbn); 
+ 
+ 
+ 
+%% plot 
+
+     subplot(2,2,1)
+    plot( AIG,   mean(AI_SPK), ' *', 'color', 'b')  
+    hold on; plot( cAIG{1,2}, eAIG, '-rd'); title('AIG')
+
+         subplot(2,2,2)
+    plot( AIA,   mean(AI_SPK), ' *', 'color', 'b')  
+    hold on; plot( cAIA{1,2}, eAIA, '-rd'); title('AIA')
+
+ 
+% 
+%n=x(1).*fspecial('gaussian', length(sz), x(2)) +x(3);
+figure
+ subplot(2,2,1)
+ surf(mt)
+ title('data')
+ 
+ subplot(2,2,2)
+ surf(reshape(Gf(3,:),50,50)+ns)
+ title('fit')
+ 
+ subplot(2,2,3)
+ surf(mt-(reshape(Gf(3,:),50,50)+ns))
+ title('data-fit')
+ 
+ subplot(2,2,4)
+ n=reshape(Gf(3,:),50,50);
+ plot(1:length(sz), [mt(round(y1),:); n(round(y1),:); mt(:,round(x1))'; n(:,round(x1))'])
+ title('x section')
+ 
+%    %% projections from orginal filter estimation
+% % % %  prjz=NI_img*NIc';
+% % % %  prj=zeros(size(prjz));
+% % % %  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
+% % % %  NIN=sum(prj,2);
+% % % %  
+% % % %  prjz=AI_img*NIc';
+% % % %  prj=zeros(size(prjz));
+% % % %  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
+% % % %  AIN=sum(prj,2);
+% % % %  
+% % % dly=8;
+% % %  prjz=AI_img*AIf';
+% % %  prj=zeros(size(prjz));
+% % %  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
+% % %  AIA=sum(prj,2);
+% % %  
+% % %  prjz=NI_img*AIf';
+% % %  prj=zeros(size(prjz));
+% % %  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
+% % %  NIA=sum(prj,2);
+% % %  
+% % %  nbn=16;
+% % % %[nNINI cNINI eNINI]=NLf082115(NIN, NI_SPK, nbn);
+% % % [nAIAI cAIAI eAIAI]=NLf082115(AIA, AI_SPK, nbn);
+% % % [nNIAI cNIAI eNIAI]=NLf082115(NIA, NI_SPK, nbn);
+% % % %[nAINI cAINI eAINI]=NLf082115(AIN, AI_SPK, nbn);
+% % %  figure(2)
+% % %  
+% % %     %subplot(2,2,1)
+% % %     %plot( NIN,   mean(NI_SPK), ' *', 'color', 'b')  
+% % %     %hold on; plot( cNINI{1,2}, eNINI, '-rd'); title('NINI')
+% % %     
+% % %     subplot(2,2,2)
+% % %     plot( AIA,   mean(AI_SPK), ' *', 'color', 'b')  
+% % %     hold on; plot( cAIAI{1,2}, eAIAI, '-rd'); title('AIAI')
+% % %     
+% % %     subplot(2,2,3)
+% % %     plot( NIA,   mean(NI_SPK), ' *', 'color', 'b')  
+% % %     hold on; plot( cNIAI{1,2}, eNIAI, '-rd'); title('NIAI')
+% % %     
+% % %     %subplot(2,2,4)
+% % %     %plot( AIN,   mean(AI_SPK), ' *', 'color', 'b')  
+% % %     %hold on; plot( cAINI{1,2}, eAINI, '-rd'); title('AINI')
+% % % 
+%    for k=1:dly
+%      figure(3); subplot(4,4,v(k)); imagesc(reshape(AIf(k,:),p_sz,p_sz)); colormap(gray); 
+%      title(['A=' num2str(prmtz(k),'%10.2f')])
+%      figure(4); subplot(4,4,v(k)); imagesc(reshape(AIc(k,:),p_sz,p_sz)); colormap(gray)
+%      figure(5); subplot(4,4,v(k)); imagesc(reshape(AI_flt(k,:),p_sz,p_sz)); colormap(gray)
+%    end
+% % %  
+% %  
+% %  
+% %   % projections from orginal filter
 % %  prjz=NI_img*NIc';
 % %  prj=zeros(size(prjz));
 % %  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
@@ -107,100 +201,46 @@ dly=16; p_sz=50;
 % %  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
 % %  AIN=sum(prj,2);
 % %  
-% dly=8;
-%  prjz=AI_img*AIf';
-%  prj=zeros(size(prjz));
-%  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
-%  AIA=sum(prj,2);
-%  
-%  prjz=NI_img*AIf';
-%  prj=zeros(size(prjz));
-%  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
-%  NIA=sum(prj,2);
-%  
-%  nbn=16;
-% %[nNINI cNINI eNINI]=NLf082115(NIN, NI_SPK, nbn);
-% [nAIAI cAIAI eAIAI]=NLf082115(AIA, AI_SPK, nbn);
-% [nNIAI cNIAI eNIAI]=NLf082115(NIA, NI_SPK, nbn);
-% %[nAINI cAINI eAINI]=NLf082115(AIN, AI_SPK, nbn);
-%  figure(2)
-%  
-%     %subplot(2,2,1)
-%     %plot( NIN,   mean(NI_SPK), ' *', 'color', 'b')  
-%     %hold on; plot( cNINI{1,2}, eNINI, '-rd'); title('NINI')
-%     
-%     subplot(2,2,2)
-%     plot( AIA,   mean(AI_SPK), ' *', 'color', 'b')  
-%     hold on; plot( cAIAI{1,2}, eAIAI, '-rd'); title('AIAI')
-%     
-%     subplot(2,2,3)
-%     plot( NIA,   mean(NI_SPK), ' *', 'color', 'b')  
-%     hold on; plot( cNIAI{1,2}, eNIAI, '-rd'); title('NIAI')
-%     
-%     %subplot(2,2,4)
-%     %plot( AIN,   mean(AI_SPK), ' *', 'color', 'b')  
-%     %hold on; plot( cAINI{1,2}, eAINI, '-rd'); title('AINI')
-% 
-%    for k=1:16
-%      figure(3); subplot(4,4,k); imagesc(reshape(AIf(k,:),p_sz,p_sz)); colormap(gray); 
-%      title(['A=' num2str(prmtz(k),'%10.2f')])
-%      figure(4); subplot(4,4,k); imagesc(reshape(AIc(k,:),p_sz,p_sz)); colormap(gray)
-%      figure(5); subplot(4,4,k); imagesc(reshape(AI_flt(k,:),p_sz,p_sz)); colormap(gray)
-%    end
-%  
- 
- 
-  % projections from orginal filter
- prjz=NI_img*NIc';
- prj=zeros(size(prjz));
- for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
- NIN=sum(prj,2);
- 
- prjz=AI_img*NIc';
- prj=zeros(size(prjz));
- for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
- AIN=sum(prj,2);
- 
- prjz=AI_img*AIc';
- prj=zeros(size(prjz));
- for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
- AIA=sum(prj,2);
- 
- prjz=NI_img*AIc';
- prj=zeros(size(prjz));
- for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
- NIA=sum(prj,2);
- 
- nbn=16;
-[nNINI cNINI eNINI]=NLf082115(NIN, NI_SPK, nbn);
-[nAIAI cAIAI eAIAI]=NLf082115(AIA, AI_SPK, nbn);
-[nNIAI cNIAI eNIAI]=NLf082115(NIA, NI_SPK, nbn);
-[nAINI cAINI eAINI]=NLf082115(AIN, AI_SPK, nbn);
- figure(1)
- 
-    subplot(2,2,1)
-    plot( NIN,   mean(NI_SPK), ' *', 'color', 'b')  
-    hold on; plot( cNINI{1,2}, eNINI, '-rd'); title('NINI')
-    
-    subplot(2,2,2)
-    plot( AIA,   mean(AI_SPK), ' *', 'color', 'b')  
-    hold on; plot( cAIAI{1,2}, eAIAI, '-rd'); title('AIAI')
-    
-    subplot(2,2,3)
-    plot( NIA,   mean(NI_SPK), ' *', 'color', 'b')  
-    hold on; plot( cNIAI{1,2}, eNIAI, '-rd'); title('NIAI')
-    
-    subplot(2,2,4)
-    plot( AIN,   mean(AI_SPK), ' *', 'color', 'b')  
-    hold on; plot( cAINI{1,2}, eAINI, '-rd'); title('AINI')
-    
-   for k=1:16
-     figure(2); subplot(4,4,k); imagesc(reshape(AI_flt(k,:),p_sz,p_sz)); colormap(gray)
-     if sum(k==v)>0.5
-     figure(3); subplot(4,4,k); imagesc(reshape(AIc(k-v(1)+1,:),p_sz,p_sz)); colormap(gray)
-     figure(4); subplot(4,4,k); imagesc(reshape(NIc(k-v(1)+1,:),p_sz,p_sz)); colormap(gray)
-     end
-   end
- 
-  
-   
+% %  prjz=AI_img*AIc';
+% %  prj=zeros(size(prjz));
+% %  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
+% %  AIA=sum(prj,2);
+% %  
+% %  prjz=NI_img*AIc';
+% %  prj=zeros(size(prjz));
+% %  for k=1:dly; prj(k:end, k)= prjz(1:end-k+1,k); end 
+% %  NIA=sum(prj,2);
+% %  
+% %  nbn=16;
+% % [nNINI cNINI eNINI]=NLf082115(NIN, NI_SPK, nbn);
+% % [nAIAI cAIAI eAIAI]=NLf082115(AIA, AI_SPK, nbn);
+% % [nNIAI cNIAI eNIAI]=NLf082115(NIA, NI_SPK, nbn);
+% % [nAINI cAINI eAINI]=NLf082115(AIN, AI_SPK, nbn);
+% %  figure(1)
+% %  
+% %     subplot(2,2,1)
+% %     plot( NIN,   mean(NI_SPK), ' *', 'color', 'b')  
+% %     hold on; plot( cNINI{1,2}, eNINI, '-rd'); title('NINI')
+% %     
+% %     subplot(2,2,2)
+% %     plot( AIA,   mean(AI_SPK), ' *', 'color', 'b')  
+% %     hold on; plot( cAIAI{1,2}, eAIAI, '-rd'); title('AIAI')
+% %     
+% %     subplot(2,2,3)
+% %     plot( NIA,   mean(NI_SPK), ' *', 'color', 'b')  
+% %     hold on; plot( cNIAI{1,2}, eNIAI, '-rd'); title('NIAI')
+% %     
+% %     subplot(2,2,4)
+% %     plot( AIN,   mean(AI_SPK), ' *', 'color', 'b')  
+% %     hold on; plot( cAINI{1,2}, eAINI, '-rd'); title('AINI')
+% %     
+% %    for k=1:16
+% %      figure(2); subplot(4,4,k); imagesc(reshape(AI_flt(k,:),p_sz,p_sz)); colormap(gray)
+% %      if sum(k==v)>0.5
+% %      figure(3); subplot(4,4,k); imagesc(reshape(AIc(k-v(1)+1,:),p_sz,p_sz)); colormap(gray)
+% %      figure(4); subplot(4,4,k); imagesc(reshape(NIc(k-v(1)+1,:),p_sz,p_sz)); colormap(gray)
+% %      end
+% %    end
+% %  
+% %   figure(1)
+% %    
---
+++ gf4.m
@@ -0,0 +1,18 @@
+function z=gf3(x, mt, ind)
+A=x(1);
+sx=x(2);
+sy=sx;
+
+x0=ind(1);
+y0=ind(2);
+
+
+[xi, yi] = meshgrid(1:length(mt));
+
+
+xp=((xi-x0).^2)./(2.*sx.^2);
+yp=((yi-y0).^2)./(2.*sy.^2);
+
+g=A.*exp(-(xp+yp));%./(2.*pi.*sx.*sy)
+
+z=sum((g(:)-mt(:)).^2);
